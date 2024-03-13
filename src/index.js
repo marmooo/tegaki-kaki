ac5a6e4b4b6da691723969f1567a1e640086630b
marmooo <marmooo@users.noreply.github.com>
2024-03-14 00:03:34 +0900

    use globalThis

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const canvasCache = document.createElement("canvas").getContext("2d", {
   alpha: false,
   willReadFrequently: true,
 });
-const audioContext = new AudioContext();
+const audioContext = new globalThis.AudioContext();
 const audioBufferCache = {};
 loadAudio("end", "mp3/end.mp3");
 loadAudio("correct", "mp3/correct3.mp3");
@@ -79,7 +79,7 @@ function unlockAudio() {
 
 function loadVoices() {
   // https://stackoverflow.com/questions/21513706/
-  const allVoicesObtained = new Promise(function (resolve) {
+  const allVoicesObtained = new Promise((resolve) => {
     let voices = speechSynthesis.getVoices();
     if (voices.length !== 0) {
       resolve(voices);
@@ -104,7 +104,7 @@ function loadVoices() {
 
 function speak(text) {
   speechSynthesis.cancel();
-  const msg = new SpeechSynthesisUtterance(text);
+  const msg = new globalThis.SpeechSynthesisUtterance(text);
   msg.voice = japaneseVoices[Math.floor(Math.random() * japaneseVoices.length)];
   msg.lang = "ja-JP";
   speechSynthesis.speak(msg);
